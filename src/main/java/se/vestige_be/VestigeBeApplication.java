229d7b8925f84465e1590452d0646d718577ea7a
Nguyen <nguyenmse181820@fpt.edu.vn>
2025-05-14 20:59:36 +0700

    init

---
+++ src/main/java/se/vestige_be/VestigeBeApplication.java
@@ -0,0 +1,13 @@
+package se.vestige_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VestigeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VestigeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/vestige_be/VestigeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package se.vestige_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VestigeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
